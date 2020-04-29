549f1c9308245cc793e567ce014dfa3f1c31df1f
kJ Lee <lee.kjlee@gmail.com>
2020-04-29 17:00:52 +0900

    Complete C Learning 9

--- C Learning/C Learning 9/C Learning 9.c
+++ C Learning/C Learning 9/C Learning 9.c
@@ -106,21 +106,117 @@
 
 // * Chapter 9_10 Fibonacci
 
-int fibonacci(int number);
+//int fibonacci(int number);
+//
+//int main()
+//{
+//	for (int count = 1; count < 13; ++count)
+//	{
+//		printf("%d ", fibonacci(count));
+//	}
+//	return 0;
+//}
+//
+//int fibonacci(int number)
+//{
+//	if (number > 2)
+//		return fibonacci(number - 1) + fibonacci(number - 2);
+//	else
+//		return 1;
+//}
+
+// * Chapter 9_13 포인터의 기본 사용법
+
+//int main()
+//{
+	/*int a, b;
+
+	a = 123;
+	int *a_ptr;
+	a_ptr = &a;
+	printf("%d %d %p\n", a, *a_ptr, a_ptr);
+
+
+	*a_ptr = 456;
+	printf("%d %d %p\n", a, *a_ptr, a_ptr);*/
+
+
+	// * Chapter 9_15 Null 포인터와 런타임에러 
+
+	//int a;
+	//printf("%p\n", &a);
+	//printf("%d\n", a);// initialization 않하면 쓰레기값이 들어간다
+
+
+
+	//int* ptr = 1234 ; //포인터 변수에 주소가 아닌 정수를 입력한 오류	                    
+
+	//printf("%p\n", ptr);
+	//printf("%d\n", *ptr);// 이때 Rederection 하는 과정에서 Runtime 오류가 발생한다
+
+
+//	//* NULL 값 넣어주는 실무형 방법
+//
+//	int* safer_ptr = NULL;
+//
+//	int a = 123;
+//
+//	//safer_ptr = &a;
+//
+//	int b;
+//	
+//	scanf("%d", &b);
+//
+//	if (b % 2 == 0)
+//		safer_ptr = &a;
+//
+//	if (safer_ptr != NULL)
+//		printf("%p\n", safer_ptr);
+//
+//	if (safer_ptr != NULL)
+//		printf("%d\n", *safer_ptr);
+//
+//}
+
+//// * Chapter 9_17 포인터변수의 크기
+//
+//void main()
+//{
+//	char a;
+//	float b;
+//	double c;
+//
+//	char* ptr_a = &a;
+//	float* ptr_b = &b;
+//	double* ptr_c = &c;
+//
+//	printf("%zd %zd %zd\n", sizeof(a), sizeof(b), sizeof(c));
+//	printf("%zd %zd %zd\n", sizeof(ptr_a), sizeof(ptr_b), sizeof(ptr_c));
+//	printf("%zd %zd %zd\n", sizeof(&a), sizeof(&b), sizeof(&c));
+//	printf("%zd %zd %zd\n", sizeof(char*), sizeof(float*), sizeof(double*));
+//
+//}
+
+// * Chapter 9_18 포인터형 매개변수
+//   Call by pointer
+
+void swap(int* u, int* v)
+{
+	printf("%p %p\n", u, v);
+
+	int temp = *u;
+	*u = *v;
+	*v = temp;
+}
 
 int main()
 {
-	for (int count = 1; count < 13; ++count)
-	{
-		printf("%d ", fibonacci(count));
-	}
-	return 0;
-}
+	int a = 123;
+	int b = 456;
 
-int fibonacci(int number)
-{
-	if (number > 2)
-		return fibonacci(number - 1) + fibonacci(number - 2);
-	else
-		return 1;
+	printf("%p %p\n", &a, &b);
+
+	swap(&a, &b);
+
+	printf("%d %d\n", a, b);
 }
